0dcef327e311b88a7e308a80f87e61e6d14d0771
idreamers <idreamers@dreamtech.tw>
2014-01-16 15:19:40 +0800

    增加開發日誌

---
+++ devlog.php
@@ -0,0 +1,106 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+    <title>匿名課程資訊</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="navbar.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">NTUE 匿名課程資訊</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php">首頁</a></li>
+            <li><a href="#coruse">課程資訊</a></li>
+            <li><a href="#coruse"> Q&A </a></li>
+
+            
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#About">關於我們</a></li>
+            <li class="active">開發日誌</li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">會員資訊 <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">個人資料</a></li>
+                <li><a href="#">網站管理</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">登出</li>
+              </ul>
+            </li>
+    
+         <form class="navbar-form navbar-right" role="form">
+            <div class="form-group">
+              <input type="text" placeholder="課程名稱或老師姓名" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">查詢</button>
+          </form> 
+   
+            
+          </ul>
+        </div>
+        <!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+    <div class="jumbotron">
+      <div class="container">
+        <h1>緣起...</h1>
+        <p>由於小弟我是轉學生，因此對很多課程的評價都不清楚...</p>
+        <p>所以一不小心就選到很雷的課，又要四處問別人評價</p>
+        <p>於是這就誕生了....</p>
+
+
+      </div>
+    </div>
+      <hr>
+
+      <footer>
+        <p>&copy; Powered By Rick Li</p>
+      </footer> 
+    </div>
+
+     <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
